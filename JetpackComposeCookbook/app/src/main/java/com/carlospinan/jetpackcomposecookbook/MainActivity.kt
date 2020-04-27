08773f53c311fbe522df2971d54e101e56b859cd
Carlos Piñan <carlos.pinan@gmail.com>
2020-04-26 21:33:41 -0500

    Added appbar sample

--- JetpackComposeCookbook/app/src/main/java/com/carlospinan/jetpackcomposecookbook/MainActivity.kt
+++ JetpackComposeCookbook/app/src/main/java/com/carlospinan/jetpackcomposecookbook/MainActivity.kt
@@ -3,31 +3,41 @@ package com.carlospinan.jetpackcomposecookbook
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.layout.Spacer
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
+import com.carlospinan.jetpackcomposecookbook.design.SimpleBottomAppBar
+import com.carlospinan.jetpackcomposecookbook.design.SimpleBottomAppBarCutoutWithScaffold
+import com.carlospinan.jetpackcomposecookbook.design.SimpleTopAppBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                // AppBarSample()
+                SimpleBottomAppBarCutoutWithScaffold()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppBarSample() {
+    Column {
+        SimpleTopAppBar()
+        Spacer(Modifier.weight(1f, true))
+        SimpleBottomAppBar()
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        Greeting("Android")
+        AppBarSample()
     }
 }
---
+++ JetpackComposeCookbook/app/src/main/java/com/carlospinan/jetpackcomposecookbook/design/AppBarSample.kt
@@ -0,0 +1,81 @@
+package com.carlospinan.jetpackcomposecookbook.design
+
+import androidx.compose.Composable
+import androidx.ui.core.Modifier
+import androidx.ui.foundation.Icon
+import androidx.ui.foundation.Text
+import androidx.ui.foundation.shape.corner.CircleShape
+import androidx.ui.layout.Spacer
+import androidx.ui.material.*
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.Favorite
+import androidx.ui.material.icons.filled.Menu
+
+/**
+ * @author Carlos Piñan
+ */
+@Composable
+fun SimpleTopAppBar() {
+    TopAppBar(
+        title = { Text("Simple TopAppBar") },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Menu)
+            }
+        },
+        actions = {
+            // RowScope here, so these icons will be placed horizontally
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Favorite)
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Favorite)
+            }
+        }
+    )
+}
+
+@Composable
+fun SimpleBottomAppBar() {
+    BottomAppBar {
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(Icons.Filled.Menu)
+        }
+        // The actions should be at the end of the BottomAppBar
+        Spacer(Modifier.weight(1f, true))
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(Icons.Filled.Favorite)
+        }
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(Icons.Filled.Favorite)
+        }
+    }
+}
+
+@Composable
+fun SimpleBottomAppBarCutoutWithScaffold() {
+    val fabShape = CircleShape
+
+    Scaffold(
+        bottomAppBar = { fabConfiguration ->
+            BottomAppBar(fabConfiguration = fabConfiguration, cutoutShape = fabShape) {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Filled.Favorite)
+                }
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Filled.Favorite)
+                }
+            }
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Click me!") },
+                shape = fabShape,
+                onClick = { /* doSomething() */ }
+            )
+        },
+        floatingActionButtonPosition = Scaffold.FabPosition.EndDocked
+    ) {
+        Text("Your app goes here")
+    }
+}
